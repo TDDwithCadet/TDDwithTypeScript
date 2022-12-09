11a1c79f50a3bbd0aff9ee8fd514856585014712
parkjoungwan <whddhs6645@gmail.com>
2022-12-09 12:44:31 +0900

    [FIN] 5장 솔직히 말하면 끝

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Dollar from './currency/Dollar'
+import Franc from './currency/Franc'
+
+describe('get result of multiplying stock by number', () => {
+  it('multipling correct with Dollar', () => {
+      const five:Dollar = new Dollar(5);
+      expect(five.times(2)).toEqual(new Dollar(10));
+      expect(five.times(3)).toEqual(new Dollar(15));
+    })
+  it('multipling correct with Franc', () => {
+      const five:Franc = new Franc(5);
+      expect(five.times(2)).toEqual(new Franc(10));
+      expect(five.times(3)).toEqual(new Franc(15));
+    })
+  it('equality correct', () => {
+     expect(new Dollar(5).equals(new Dollar(5))).toEqual(true);
+     expect(new Dollar(5).equals(new Dollar(6))).toEqual(false);
+  })
+})
 
 test('renders learn react link', () => {
   render(<App />);
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Dollar from './currency/Dollar'
-
-
-describe('get result of multiplying stock by number', () => {
-  it('multipling correct', () => {
-      const five:Dollar = new Dollar(5);
-      expect(five.times(2)).toEqual(new Dollar(10));
-      expect(five.times(3)).toEqual(new Dollar(15));
-    })
-  it('equality correct', () => {
-     expect(new Dollar(5).equals(new Dollar(5))).toEqual(true);
-     expect(new Dollar(5).equals(new Dollar(6))).toEqual(false);
-  })
-})
 
 function App() {
   return (
